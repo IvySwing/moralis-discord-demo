ed724d78b012f9a3364f06169ca793f8c7d9a284
IvySwing <entense@att.net>
2022-12-20 22:08:30 +0000

    created discord bot to display transfer notifications

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const Moralis = require("moralis").default;
+const discord = require("discord.js");
+require("dotenv").config();
+const app = express();
+const port = 3000;
+
+const client = new discord.Client({
+  intents: [],
+});
+
+client.login(process.env.PASS);
+
+app.use(express.json());
+
+app.post("/webhook", async (req, res) => {
+  const { body, headers } = req;
+
+  //console.log(body);
+
+  try {
+    Moralis.Streams.verifySignature({
+      body,
+      signature: headers["x-signature"],
+    });
+
+    let from = body.erc20Transfers[0].from;
+    let amount = Number(body.erc20Transfers[0].valueWithDecimals);
+
+    const channel = await client.channels.fetch(process.env.CHANNEL);
+    channel.send(`Transfer submitted by ${from}, for ${amount.toFixed(2)}`);
+
+    return res.status(200).json();
+  } catch (e) {
+    console.log("Not Moralis");
+    return res.status(400).json();
+  }
+});
+
+Moralis.start({
+  apiKey: process.env.APIKEY,
+}).then(() => {
+  app.listen(port, () => {
+    console.log(`Listening to my stream`);
+  });
+});
