c5a9e973478a30a3bf987cec615c43f72fb878da
IvySwing <entense@att.net>
2022-12-30 17:48:07 +0000

    code cleanup

--- index.js
+++ index.js
@@ -19,12 +19,6 @@ app.post("/webhook", async (req, res) => {
   const { body, headers } = req;
 
   try {
-    // const streams = await Moralis.Streams.getAll({
-    //   limit: 100, // limit the number of streams to return
-    // });
-
-    //console.log(streams);
-
     const AVAX_CHAIN_ID = 43114;
     const POLYGON_CHAIN_ID = 137;
     const OPTIMISM_CHAIN_ID = 10;
@@ -45,21 +39,13 @@ app.post("/webhook", async (req, res) => {
     // console.log(headers);
     // console.log(body);
 
-    //let from = body.erc20Transfers[0].from;
-    // console.log(from);
-    // let amount = Number(body.erc20Transfers[0].valueWithDecimals);
-    // console.log(amount);
-
     let from = body.txs[0].fromAddress;
-    console.log(from);
+    //console.log(from);
     let tokenId = web3.utils.hexToNumber(body.logs[0].topic1);
-    console.log(tokenId);
+    //console.log(tokenId);
     let chainId = web3.utils.hexToNumber(body.chainId);
-    console.log(chainId);
+    //console.log(chainId);
     let chain = chainIdLookup[chainId];
-
-    // const channel = await client.channels.fetch(process.env.CHANNEL);
-    // channel.send(`Transfer submitted by ${from}, for ${amount.toFixed(2)}`);
 
     const channel = await client.channels.fetch(process.env.CHANNEL);
     channel.send(`Token ID ${tokenId} claimed by ${from} from ${chain}`);
